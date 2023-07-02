3f4fb96a982719ad525e04fdb52ffe66f28715df
Nirbhay Tejani <nirbhaytejani1234@gmail.com>
2023-07-03 00:43:03 +0530

    Add files via upload

---
+++ src/serve_clean/src/PathPlanningNode.cpp
@@ -0,0 +1,37 @@
+#include "CleaningPathPlanner.h"
+#include <boost/shared_ptr.hpp>
+#include <costmap_2d/costmap_2d_ros.h>
+
+namespace cm = costmap_2d;
+namespace rm = geometry_msgs;
+
+using std::vector;
+using rm::PoseStamped;
+using std::string;
+using cm::Costmap2D;
+using cm::Costmap2DROS;
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "path_planning_node");//hjr
+
+    tf2_ros::Buffer tf;
+    tf2_ros::TransformListener tf2_listener(tf);
+    costmap_2d::Costmap2DROS lcr("cleaning_costmap", tf);
+    //planner_costmap_ros_->pause();
+
+    ros::Duration(5).sleep();
+    CleaningPathPlanning clr(&lcr);
+    clr.GetPathInROS();
+    //clr.GetBorderTrackingPathInROS();   
+    ros::Rate r(1);
+    while(ros::ok()){       
+      clr.PublishCoveragePath();
+      ros::spinOnce();
+      r.sleep();
+    }
+
+    ros::shutdown();
+    return 0;
+}
+
---
+++ src/serve_clean/src/next_goal.cpp
@@ -0,0 +1,220 @@
+
+#include <ros/ros.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "nav_msgs/Odometry.h"
+#include "nav_msgs/Path.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <tf/transform_listener.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace tf;
+
+float x_current;
+float y_current;
+
+float normeNextGoal;
+
+class quaternion_ros
+{
+public:
+  float w;
+  float x;
+  float y;
+  float z;
+
+  quaternion_ros();
+
+  void toQuaternion(float pitch, float roll, float yaw);
+};
+
+void quaternion_ros::toQuaternion(float pitch, float roll, float yaw)
+{
+
+  float cy = cos(yaw * 0.5);
+  float sy = sin(yaw * 0.5);
+  float cr = cos(roll * 0.5);
+  float sr = sin(roll * 0.5);
+  float cp = cos(pitch * 0.5);
+  float sp = sin(pitch * 0.5);
+
+  w = cy * cr * cp + sy * sr * sp;
+  x = cy * sr * cp - sy * cr * sp;
+  y = cy * cr * sp + sy * sr * cp;
+  z = sy * cr * cp - cy * sr * sp;
+}
+
+quaternion_ros::quaternion_ros()
+{
+  w = 1;
+  x = 0;
+  y = 0;
+  z = 0;
+}
+
+class Path_planned
+{
+public:
+  struct Goal
+  {
+    float x;
+    float y;
+    bool visited;
+  };
+
+  vector<Goal> Path;
+
+  Path_planned();
+  //Path_planned(float x, float y, bool visited);
+  void addGoal(float X, float Y, bool visit);
+};
+
+Path_planned::Path_planned()
+{
+}
+
+//Path_planned(float x, float y, bool visited)
+
+void Path_planned::addGoal(float X, float Y, bool visit)
+{
+  Path_planned::Goal newGoal;
+  newGoal.x = X;
+  newGoal.y = Y;
+  newGoal.visited = visit;
+  Path.push_back(newGoal);
+}
+
+Path_planned planned_path;
+nav_msgs::Path passed_path;
+ros::Publisher pub_passed_path;
+void pose_callback(const nav_msgs::Odometry &poses)
+{ 
+  x_current = poses.pose.pose.position.x;
+  y_current = poses.pose.pose.position.y;
+  passed_path.header = poses.header;
+  geometry_msgs::PoseStamped p;
+  p.header = poses.header;
+  p.pose = poses.pose.pose;
+  passed_path.poses.emplace_back(p);
+  pub_passed_path.publish(passed_path);
+}
+
+int taille_last_path = 0;
+bool new_path = false;
+
+
+void path_callback(const nav_msgs::Path &path)
+{
+  
+  if ((planned_path.Path.size() == 0) || (path.poses.size() != taille_last_path))
+  {
+    planned_path.Path.clear();
+    new_path = true;
+    for (int i = 0; i < path.poses.size(); i++)
+    {
+      planned_path.addGoal(path.poses[i].pose.position.x, path.poses[i].pose.position.y, false);
+
+      cout << path.poses[i].pose.position.x << " " << path.poses[i].pose.position.y << endl;
+    }
+    cout << "Recv path size:" << path.poses.size() << endl;
+    taille_last_path = path.poses.size();
+  }
+}
+
+// int **count_antonin(char *)
+
+int main(int argc, char *argv[])
+{
+  srand(time(0));
+  ros::init(argc, argv, "next_goal");
+  ros::NodeHandle next_goal;
+  ros::Subscriber sub1 = next_goal.subscribe("/odom", 1000, pose_callback);
+  ros::Subscriber sub2 = next_goal.subscribe("/path_planning_node/cleaning_plan_nodehandle/cleaning_path", 1000, path_callback);
+
+  ros::Publisher pub1 = next_goal.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1000);
+  pub_passed_path = next_goal.advertise<nav_msgs::Path>("/clean_robot/passed_path", 1000);
+
+  ros::Rate loop_rate(10);
+
+  geometry_msgs::PoseStamped goal_msgs;
+  int count = 0;
+  double angle;
+  bool goal_reached = false;
+  
+  if (!next_goal.getParam("/NextGoal/tolerance_goal", normeNextGoal))
+  {
+    ROS_ERROR("Please set your tolerance_goal");
+    return 0;
+  }
+  ROS_INFO("tolerance_goal=%f", normeNextGoal);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    if (new_path)
+    {
+      count = 0;
+      new_path = false;
+    }
+    
+    cout << " count : " << count << endl;
+    if (!planned_path.Path.empty())
+    {
+      
+      if (sqrt(pow(x_current - planned_path.Path[count].x, 2) + pow(y_current - planned_path.Path[count].y, 2)) <= normeNextGoal)
+      {
+        count++;
+        goal_reached = false;
+      }
+      if (goal_reached == false)
+      {
+        goal_msgs.header.frame_id = "odom";
+        goal_msgs.header.stamp = ros::Time::now();
+        goal_msgs.pose.position.x = planned_path.Path[count].x;
+        goal_msgs.pose.position.y = planned_path.Path[count].y;
+        goal_msgs.pose.position.z = 0;
+        if (count < planned_path.Path.size())
+        {
+          angle = atan2(planned_path.Path[count + 1].y - planned_path.Path[count].y, planned_path.Path[count + 1].x - planned_path.Path[count].x);
+        }
+        else
+        {
+          angle = atan2(planned_path.Path[0].y - planned_path.Path[count].y, planned_path.Path[0].x - planned_path.Path[count].x);
+        }
+        cout << angle << endl;
+        quaternion_ros q;
+        q.toQuaternion(0, 0, float(angle));
+        goal_msgs.pose.orientation.w = q.w;
+        goal_msgs.pose.orientation.x = q.x;
+        goal_msgs.pose.orientation.y = q.y;
+        if (planned_path.Path[count].x < planned_path.Path[count + 1].x)
+        {
+          goal_msgs.pose.orientation.z = 0;
+        }
+        if (planned_path.Path[count].x > planned_path.Path[count + 1].x)
+        {
+          goal_msgs.pose.orientation.z = 2;
+        }
+
+        cout << " NEW GOAL " << endl;
+        cout << " x = " << planned_path.Path[count].x << " y = " << planned_path.Path[count].y << endl;
+
+        goal_reached = true;
+        pub1.publish(goal_msgs);
+      }
+      cout << x_current << " " << y_current << endl;
+      
+      cout << planned_path.Path[count].x << " " << planned_path.Path[count].y << endl;
+    
+      cout << " DISTANCE : " << sqrt((x_current - planned_path.Path[count].x) * (x_current - planned_path.Path[count].x) + (y_current - planned_path.Path[count].y) * (y_current - planned_path.Path[count].y)) << endl;
+     
+    }
+
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
